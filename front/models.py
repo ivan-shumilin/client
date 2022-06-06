8e754367dd44909c3826ca068cc87256766661fe
ivan-shumiln <shumilin.i.v@gmail.com>
2022-06-06 16:53:49 +0300

    Подготовка к деплою.

--- front/models.py
+++ front/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Forecast(models.Model):
+    sing = models.CharField(max_length=200, null=True)
+
+
+    def __str__(self):
+        return f'{self.sing}'
